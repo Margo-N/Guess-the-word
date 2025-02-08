6fb1975819c9212923bf4c335f3825bcbb458a7b
margo_n <margoneff3@gmail.com>
2025-02-08 14:35:22 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const guessedLettersList = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesParagraph = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+// Show symbols as placeholders for the letters of the chosen word
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+}
+
+placeholder(word);
+
+guessButton.addEventListener ("click", function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+});

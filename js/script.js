0ab08eaea9e36ce46b46189a766908b2c3bebf5a
margo_n <margoneff3@gmail.com>
2025-02-22 17:58:12 -0500

    Displayed word and guessed letters

--- js/script.js
+++ js/script.js
@@ -62,6 +62,40 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
     }
-    };
+};
 
+const showGuessedLetters = function () {
+    //Clear the list
+    guessedLettersList.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersList.append(li);
+}
+};
+
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    console.log(wordArray);
+    const showWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            showWord.push(letter.toUpperCase());
+         } else {
+            showWord.push("●");
+         }
+    }
+wordInProgress.innerText = showWord.join("");
+checkIfWon();
+};
+
+const checkIfWon = function () {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;   
+    }
+};
